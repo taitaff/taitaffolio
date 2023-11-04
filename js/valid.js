efcab38d7108a476cd0e9dc1722b38b1695d5cc7
taitaff <73388679+taitaff@users.noreply.github.com>
2023-11-04 21:55:57 +0300

    Add files via upload

---
+++ js/valid.js
@@ -0,0 +1,21 @@
+(() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+        if (form.checkValidity()) {
+          const toast = new bootstrap.Toast(toastLiveExample)
+          toast.show()
+        }
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
